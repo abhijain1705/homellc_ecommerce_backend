481cd962c97d561642ba3c34751390370182d07e
Abhi jain <91718608+abhijain2003@users.noreply.github.com>
2023-03-10 19:24:42 +0530

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const routes = express.Router();
-const {AddUser, GetCurrentUser} = require("../controllers/userContoller");
+const { AddUser, LoginUser } = require("../controllers/userContoller");
 
-routes.get("/login", GetCurrentUser);
+routes.post("/login", LoginUser);
 routes.post("/signup", AddUser);
 
 module.exports = routes;
